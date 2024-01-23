de3c099b82cb639cd3f4fbdbc3ed2ccfb42f64e1
muta <timirgfleev@gmail.com>
2024-01-24 02:05:06 +0300

    Make between user chat

--- server/chat.h
+++ server/chat.h
@@ -1,17 +1,26 @@
 #pragma once
 #include <list>
-#include <string>
-#include <nlohmann/json.hpp>
 
-class Message{
+#include <string>
+#include <vector>
+
+#include <nlohmann/json.hpp>
+#include <mutex>
+
+#include <ctime>
+class Message
+{
 public:
-    explicit Message(nlohmann::json msg);
+    explicit Message(const std::string &msg, int user_id)
+        : user_id(user_id), timestamp(std::time(nullptr)), text(msg){
+
+                                                           };
     ~Message(){};
 
     int id;
     int user_id;
     std::string text;
-    long long int timestamp;
+    time_t timestamp;
 };
 
 class Chat
@@ -20,45 +29,66 @@ public:
     explicit Chat(){};
     ~Chat(){};
 
-    void add_message(Message message){
+    void add_message(Message message)
+    {
         messages.push_back(message);
     };
-    void add_user(int user_id){
+    void add_user(int user_id)
+    {
         users.push_back(user_id);
     };
 
-    std::list<Message> get_messages(){
-        return messages;
+    std::vector<Message> get_messages()
+    {
+        return std::vector<Message>{std::begin(messages), std::end(messages)};
+    }
+
+    bool has_user(int user_id)
+    {
+        return std::count(users.begin(), users.end(), user_id);
     }
 
     std::list<Message> messages;
     std::list<int> users;
+
 private:
     int id;
 };
 
-class ChatHandler{
+class ChatHandler
+{
 public:
-    explicit ChatHandler(){};
+    explicit ChatHandler(){
+        chats.push_back(Chat());
+    };
+
     ~ChatHandler(){};
 
-    void add_chat(Chat chat){
+    void add_chat(Chat chat)
+    {
+        std::lock_guard<std::mutex> lock(chats_mutex);
         chats.push_back(chat);
     }
 
-    void handle_message(const Message& message){
-        chats.at(message.id).add_message(message);
+    void handle_message(const Message &message, int chat_id)
+    {
+        std::lock_guard<std::mutex> lock(chats_mutex);
+        chats.at(chat_id).add_message(message);
     }
-    
-    void handle_user_chat_add(int chat_id, int user_id){
+
+    void handle_user_chat_add(int chat_id, int user_id)
+    {
+        std::lock_guard<std::mutex> lock(chats_mutex);
         chats.at(chat_id).add_user(user_id);
     }
 
-    void get_chat_messages(int chat_id){
-        chats.at(chat_id).get_messages();
+    std::vector<Message> get_chat_messages(int chat_id)
+    {
+        std::lock_guard<std::mutex> lock(chats_mutex);
+        return chats.at(chat_id).get_messages();
     }
 
+private:
     std::vector<Chat> chats;
+    std::mutex chats_mutex;
 };
-
-
--- server/client_hander.h
+++ server/client_hander.h
@@ -12,14 +12,17 @@ using boost::asio::ip::tcp;
 #include "chat.h"
 
 static ChatHandler chatHandler;
+static int user_id_gererator = 0;
 
-
-void handle_request_message(const std::string &data)
+void handle_request_message(const std::string &data, int user_id, int chat_id)
 {
     std::cout << "Message " << data << std::endl;
+
+    Message message(data, user_id);
+    chatHandler.handle_message(message, chat_id);
 };
 
-std::string handle_string(std::string &data)
+std::string handle_string(std::string &data, int user_id)
 {
     // Remove the newline character at the end of the data
     if (!data.empty() && data[data.length() - 1] == '\n')
@@ -31,21 +34,29 @@ std::string handle_string(std::string &data)
 
     std::string type = jsonData["action"];
 
+    std::cout << "input Json contains " << jsonData << std::endl;
+
+    nlohmann::json response;
     if (type == "message")
     {
-        std::string message = jsonData["message"];
-        handle_request_message(message);
-    }
+        handle_request_message(jsonData["message"], user_id, jsonData["chat_id"]);
 
-    std::string response = jsonData.dump();
-
-    return response;
+    } else if(type == "get_messages") {
+        auto messages = chatHandler.get_chat_messages(jsonData["chat_id"]);
+        
+        for(auto &message : messages){
+            response["messages"].push_back(message.text);
+        }
+    } 
+    std::cout << "response Json contains " << response << std::endl;
+    return response.dump();
 }
 
 void handle_client(tcp::socket socket)
 {
     try
     {
+        int user_id = user_id_gererator++;
 
         for (;;)
         {
@@ -57,8 +68,13 @@ void handle_client(tcp::socket socket)
             std::string data = boost::asio::buffer_cast<const char *>(buf.data());
 
             // Craft a responce
-            std::string message = handle_string(data);
+            std::string message = handle_string(data, user_id);
 
+            std::cout << "response message " << message << std::endl;
+            if(message == "null"){
+                continue;
+            }
+            
             // Write a reply to the client
             boost::system::error_code ignored_error;
             boost::asio::write(socket, boost::asio::buffer(message), ignored_error);
